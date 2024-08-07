8cb29c14f8933a397ce71674d33cdd1b644bf8e7
Tyler Ofreneo <tylerofreneo@gmail.com>
2024-08-06 19:38:47 -0700

    scuffed but functional song search

--- app/ui/songsearch.js
+++ app/ui/songsearch.js
@@ -4,76 +4,48 @@ import clsx from 'clsx'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const songs = [
-    { id: 1, name: 'Mo bamba' },
-    { id: 2, name: 'Fortnite' },
-    { id: 3, name: 'Sickomode' },
-    { id: 4, name: 'Beepboop' },
-    { id: 5, name: 'DevonWebb' },
-    { id: 6, name: 'DevonWebb' },
-    { id: 7, name: 'DevonWebb' },
-    { id: 8, name: 'DevonWebb' },
-    { id: 9, name: 'DevonWebb' },
-    { id: 10, name: 'DevonWebb' },
-    { id: 11, name: 'DevonWebb' },
-    { id: 12, name: 'DevonWebb' },
-    { id: 13, name: 'DevonWebb' },
-    { id: 14, name: 'DevonWebb' },
-    { id: 15, name: 'DevonWebb' },
-    { id: 16, name: 'DevonWebb' },
-    { id: 17, name: 'DevonWebb' },
-    { id: 18, name: 'DevonWebb' },
-    { id: 19, name: 'DevonWebb' },
-]
-
 const songs2 = []
 
 export default function SongSearch() {
-    const [query, setQuery] = useState('  ')
-    const [selected, setSelected] = useState('')
-
-
-
-    const filteredSongs =
-        query === ''
-            ? songs.slice(0, 8)
-            : songs.filter((song) => {
-                return song.name.toLowerCase().includes(query.toLowerCase())
-            }).slice(0, 8)
+    const [query, setQuery] = useState('')
+    const [selected, setSelected] = useState({})
 
     const fetchSongs = () => {
         axios
             .get(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${query}&api_key=93b076b0e136a204f45a69292934aade&format=json`)
             .then(response => {
+                console.log('searching w query ' + query)
+                
                 songs2.splice(0, songs2.length)
-                // for (let i = 0; i < 8; i++) {
-                    songs2.push({
-                        id: 0, name: response.data.results.trackmatches.track[0].name, artist: response.data.results.trackmatches.track[0].artist, 
-                        searchtags: response.data.results.trackmatches.track[0].name + response.data.results.trackmatches.track[0].artist
-                    })
-                // }
-
-                // console.log(response.data.results.trackmatches.track[i])
+                if (query != '') {
+                    let length = Object.keys(response.data.results.trackmatches.track).length
+                    console.log('found ' + length)
+                    for (let i = 0; i < length; i++) {
+                        let name = response.data.results.trackmatches.track[i].name
+                        let artist = response.data.results.trackmatches.track[i].artist
+                        songs2.push({
+                            id: name + ' - ' + artist, name: name + ' - ' + artist, artist: artist,
+                            searchtags: response.data.results.trackmatches.track[i].name + response.data.results.trackmatches.track[i].artist
+                        })
+                    }
+                }
             })
     }
 
+    // On close modal, don't forget to clear songlist
     fetchSongs()
-
-    // useEffect(() => {
-    //     fetchSongs()
-    // }, [])
 
     const filteredSongs2 =
         query === ''
-            ? songs2.slice(0, 8)
+            ? songs2.splice(0, 8)
             : songs2.filter((song) => {
-                return song.searchtags.toLowerCase().includes(query.toLowerCase().replace(/ +/g,""))
+                return song.searchtags.toLowerCase().replace(/ +/g, "").includes(query.toLowerCase().replace(/ +/g, ""))
             }).slice(0, 8)
-    
+
 
     return (
         <div className="mx-auto h-full w-full pt-4">
-            <Combobox value={selected} onChange={(value) => setSelected(value)} onClose={() => setQuery(query)} __demoMode>
+            <Combobox immediate value={selected} onChange={(value) => {setSelected(value)}} onClose={() => {setQuery(query); fetchSongs()}} __demoMode>
                 <div className="relative">
                     <ComboboxInput
                         spellCheck='false'
@@ -82,7 +54,7 @@ export default function SongSearch() {
                             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
                         )}
                         displayValue={(person) => person?.name}
-                        onChange={(event) => setQuery(event.target.value)}
+                        onChange={(event) => {setQuery(event.target.value); fetchSongs()}}
                     />
                     <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
                         <ChevronDownIcon className="size-4 fill-black group-data-[hover]:fill-black" />
@@ -104,7 +76,7 @@ export default function SongSearch() {
                             className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
                         >
                             <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-                            <div className="text-sm/6 text-black">{person.name} - {person.artist}</div>
+                            <div className="text-sm/6 text-black">{person.name}</div>
                         </ComboboxOption>
                     ))}
                 </ComboboxOptions>
